19844c6ac2ee6c1f9f2c216a7ee7a84861de4738
Gustavo Carvalho <gustavo.dev17@outlook.com>
2026-04-13 17:03:38 -0300

    conectando langsmith

--- agente.py
+++ agente.py
@@ -2,7 +2,12 @@ import os
 from dotenv import load_dotenv
 from rich import print
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
 
-load_dotenv
 
+load_dotenv()
 llm = ChatOpenAI(model=os.getenv("OPENAI_MODEL"))
+
+
+
+print(llm.invoke("Olá langsmith"))
